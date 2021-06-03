4f6fecf8f7ca96facb3764318170539d28b23d95
zxcqzw <qiaoziwei1998@gmail.com>
2021-06-03 14:18:33 +0900

    change

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,11 @@
 #include　<iostream>
 
+int sum(int, int);
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    int a = 9;
+    int b = 1;    
+    std::cout << sum(a,b) << std::endl;
     return 0;
 }
